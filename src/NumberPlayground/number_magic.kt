bca64fc3e3c104d3c6ff102db71f817268bf8af5
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-16 00:36:49 +0300

    Finished part 1: added a functions that calculates the sum of all the values in an array

--- src/NumberPlayground/number_magic.kt
+++ src/NumberPlayground/number_magic.kt
@@ -8,6 +8,11 @@ fun main() {
     // part 2
     println("\n\nPart 2");
     countDownwards(1, 10);
+
+    // part 3
+    println("\n\nPart 3");
+    val numbers: IntArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    println("[*] sum of 1 through 10: ${calculateSum(numbers)}");
 }
 
 fun countUpwards(start: Int, end: Int) {
@@ -32,3 +37,12 @@ fun countDownwards(start: Int, end: Int) {
     }
 }
 
+
+fun calculateSum(numbers: IntArray): Int {
+    var sum: Int = 0;
+    for(number in numbers) {
+        var temp: Int = numbers[number-1];
+        sum += temp;
+    }
+    return sum;
+}
