9fd9577ceac9438f60a6e37a212ff52be00c8b07
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-16 00:35:48 +0300

    Finished part 1: added a functions that prints a lists values downwards

--- src/NumberPlayground/number_magic.kt
+++ src/NumberPlayground/number_magic.kt
@@ -5,12 +5,26 @@ fun main() {
     println("Part 1");
     countUpwards(1, 10);
 
+    // part 2
+    println("\n\nPart 2");
+    countDownwards(1, 10);
 }
 
 fun countUpwards(start: Int, end: Int) {
     print("[*] ");
     for (i in start..end) {
         if(i != 1) {
+            print(", $i");
+        } else {
+            print(i);
+        }
+    }
+}
+
+fun countDownwards(start: Int, end: Int) {
+    print("[*] ");
+    for (i in end downTo start) {
+        if(i != 10) {
             print(", $i");
         } else {
             print(i);
