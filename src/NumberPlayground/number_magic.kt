c83f2ac66fe566f03a723352d61f6c64b14f78be
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-16 00:35:00 +0300

    Finished part 1: added a functions that prints a lists values upwards

---
+++ src/NumberPlayground/number_magic.kt
@@ -0,0 +1,20 @@
+package NumberPlayground
+
+fun main() {
+    // part 1
+    println("Part 1");
+    countUpwards(1, 10);
+
+}
+
+fun countUpwards(start: Int, end: Int) {
+    print("[*] ");
+    for (i in start..end) {
+        if(i != 1) {
+            print(", $i");
+        } else {
+            print(i);
+        }
+    }
+}
+
